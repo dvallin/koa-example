059b5d46f39a1f9a7c25da9086c89fe67595d6c0
Maximilian Schuler <schuler.maximilian@gmail.com>
2019-09-13 15:10:00 +0200

    fix run script

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,9 +1,5 @@
-import { build, startApp, startMode } from '../src/server'
-
-import * as request from 'supertest'
-import { Server } from 'http'
+import { startMode } from '../src/server'
 import { testing } from './test-mode'
-import { Mode } from '../src/mode'
 import { MockPostgres } from './database/mock-database'
 
 describe('app', () => {
